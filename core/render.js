85fdabc318420cff7bb2477e85b4963dc3007494
Zhang Linfeng <zhanglinfengcs@outlook.com>
2023-04-12 15:34:11 +0800

    feat: concurrent mode

--- core/render.js
+++ core/render.js
@@ -2,18 +2,43 @@ let count = 0;
 
 function render(element, container) {
   const dom =
-    element.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
+    element.type === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
       : document.createElement(element.type);
-  const isProperty = key => key !== "children";
+  const isProperty = (key) => key !== 'children';
   Object.keys(element.props)
     .filter(isProperty)
-    .forEach(name => {
+    .forEach((name) => {
       dom[name] = element.props[name];
     });
-  element.props.children.forEach(child => render(child, dom));
+  element.props.children.forEach((child) => render(child, dom));
   container.appendChild(dom);
 }
 
+// 下一个渲染单元
+let nextUnitOfWork = null;
 
-export default render
+function workLoop(deadline) {
+  // 是否应该终止渲染
+  shouldYield = false;
+
+  // 如果 持续有渲染单元 且 不应终止渲染
+  while (nextUnitOfWork && !shouldYield) {
+    // 执行并取得下一渲染单元
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    // 判断线程是否繁忙, 繁忙则终止渲染
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  // 重新请求
+  requestIdleCallback(workLoop);
+}
+
+// 请求在空闲时执行渲染
+requestIdleCallback(workLoop);
+
+// 执行渲染单元 并 返回下一渲染单元
+function performUnitOfWork(nextUnitOfWork) {
+  // TODO
+}
+
+export default render;
